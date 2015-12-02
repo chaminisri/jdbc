a610d4728d3d61a2b0ae915e51f1aef9e357ffd9
shahbazkhan <kshahbazk@yahoo.com>
2015-12-01 16:46:06 -0800

    finished functionality

--- src/main/java/Books.java
+++ src/main/java/Books.java
@@ -1,42 +1,239 @@
 /**
  * Created by shahbazkhan on 11/30/15.
+ *  This is the main class for the JDBC project.
  */
 import java.sql.*;
 public class Books {
 
+    // The Driver and the Database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/Books";
+    static final String DB_URL = "jdbc:mysql://localhost/";
 
     //  Database credentials
     static final String USER = "root";
-    static final String PASS = "root";
+    static final String PASS = "brolly91";
 
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
         try{
-            //STEP 2: Register JDBC driver
+
+            // We initialize the MYSQL database with the JDBC drivers here
+
+            // We Register the JDBC driver
             Class.forName(JDBC_DRIVER);
 
-            //STEP 3: Open a connection
+            //We Open a connection
             System.out.println("Connecting to database...");
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-            //STEP 4: Execute a query
+            // Execute a query
             System.out.println("Creating database...");
             stmt = conn.createStatement();
 
-            stmt.execute("DROP DATABASE books");
+            // Just trying to confirm that we don't get an Database exists error
+           // stmt.execute("DROP DATABASE books");
 
-            String sql = "CREATE DATABASE Books";
+            // We Create the Books database
+            String sql = "CREATE DATABASE books";
             stmt.executeUpdate(sql);
             System.out.println("Database created successfully...");
 
-            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/books" , "root" , "root");
+            // We establish a Connection to the database with JDBC
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/books" , USER , PASS);
             Statement myStmt = myConn.createStatement();
 
-            tableGenerator tc= new tableGenerator(myStmt,myConn);
-            tc.createTable();
+            // Using the Table Generator class to generate our tables
+            tableGenerator tabGen= new tableGenerator(myStmt,myConn);
+
+            // Create and initialize the tables with random values
+            tabGen.createTable();
+            tabGen.autoPopulate();
+            System.out.println();
+            System.out.println();
+
+
+
+            /**
+             *   All of the queries are listed below
+             */
+
+
+            // Select all authors from the authors table. Order the information
+            // alphabetically by the author’s last name and first name.
+
+            sqlQueryResult queryPrinter = new sqlQueryResult(DB_URL , USER , PASS);
+
+            ResultSet query1 = queryPrinter.getResultSet("SELECT * FROM Books.authors ORDER BY last DESC");
+
+            System.out.println("For the query: Select all authors from the authors table. Order the information\n" +
+                    "alphabetically by the author’s last name and first name");
+            System.out.println();
+            System.out.println("The records selected are:");
+            System.out.println();
+            int rowCount = 0;
+            System.out.println("AuthorID" + ", " + "First" + ", " + "Last");
+            while(query1.next()) {   // Move the cursor to the next row
+                int id = query1.getInt("authorID");
+                String first = query1.getString("first");
+                String last = query1.getString("last");
+                System.out.println(id + ", " + first + ", " + last);
+                ++rowCount;
+            }
+            System.out.println();
+            System.out.println();
+
+
+            // Select all publishers from the publishers table
+
+            ResultSet query2 = queryPrinter.getResultSet("SELECT * FROM Books.publishers");
+
+            System.out.println("For the query: Select all publishers from the publishers table");
+            System.out.println();
+            System.out.println("The records selected are:");
+            System.out.println();
+
+            System.out.println("publisherID" + ", " + "publisherName");
+            while(query2.next()) {   // Move the cursor to the next row
+                int id = query2.getInt("publisherID");
+                String publisherName = query2.getString("publisherName");
+                System.out.println(id + ", " + publisherName);
+                ++rowCount;
+            }
+            System.out.println();
+            System.out.println();
+
+
+            // Select a specific publisher and list all books published by that
+            // publisher. Include the title, year and ISBN number. Order the information alphabetically by title.
+
+            System.out.println("For the query: Select a specific publisher and list all books published by that\n" +
+                    "publisher. Include the title, year and ISBN number. Order the information alphabetically by title");
+            System.out.println();
+            System.out.println("The records selected are:");
+            System.out.println();
+            for(int i =1; i<15; i++){
+                ResultSet query3 = queryPrinter.getResultSet("SELECT isbn, title, copyright FROM books.titles WHERE publisherID ="+i+"");
+                while(query3.next()){
+                    System.out.println(query3.getString("isbn") + " , " +  query3.getString("title") + " , " +  query3.getString("copyright"));
+                }
+
+            }
+            System.out.println();
+            System.out.println();
+
+
+
+            // Add new author
+
+            queryPrinter.setResult("INSERT INTO books.authors (first, last) VALUES ('Shahbaz', 'Khan');");
+            // Print out author table with the recently added Author
+            System.out.println("For the query: Add new author");
+            System.out.println();
+            System.out.println("The records selected are:");
+            System.out.println();
+
+            ResultSet query4 = queryPrinter.getResultSet("SELECT * FROM Books.authors");
+
+            System.out.println("For the addition: We print out authors table after the additon");
+            System.out.println();
+            System.out.println("The updated records are:");
+            System.out.println();
+            int rowCount1 = 0;
+            System.out.println("AuthorID" + ", " + "First" + ", " + "Last");
+            while(query4.next()) {   // Move the cursor to the next row
+                int id = query4.getInt("authorID");
+                String first = query4.getString("first");
+                String last = query4.getString("last");
+                System.out.println(id + ", " + first + ", " + last);
+                ++rowCount1;
+            }
+            System.out.println();
+            System.out.println();
+
+
+            // Edit/Update the existing information about an author
+            queryPrinter.setResult("UPDATE books.authors SET first = \"Johnny\" WHERE authorID = 1");
+
+            // Print out Author table with updated value
+            ResultSet query5 = queryPrinter.getResultSet("SELECT * FROM Books.authors");
+
+            System.out.println("For the update: Edit/Update the existing information about an author");
+            System.out.println();
+            System.out.println("The updated records are:");
+            System.out.println();
+            int rowCount2 = 0;
+            System.out.println("AuthorID" + ", " + "First" + ", " + "Last");
+            while(query5.next()) {   // Move the cursor to the next row
+                int id = query5.getInt("authorID");
+                String first = query5.getString("first");
+                String last = query5.getString("last");
+                System.out.println(id + ", " + first + ", " + last);
+                ++rowCount1;
+            }
+            System.out.println();
+            System.out.println();
+
+
+            // Add a new title for an author
+
+            queryPrinter.setResult("INSERT INTO books.titles (isbn, title, editionNumber, price)\n" +
+                    " VALUES (\"890809809\",\"Book\", \"1234\", \"50\");");
+
+            System.out.println("For the update: Add a new title for an author");
+            System.out.println();
+            System.out.println("The updated records are:");
+            System.out.println();
+            System.out.println("The new title is now added");
+
+
+            // Add new publisher
+
+            queryPrinter.setResult("INSERT INTO books.publishers (publisherName) VALUES (\"Shahbaz\");");
+            // Print out the publishers Table with the new publisher added.
+            ResultSet query6 = queryPrinter.getResultSet("SELECT * FROM Books.publishers");
+
+            System.out.println("For the addition: We print out publishers table after the addition");
+            System.out.println();
+            System.out.println("The updated records are:");
+            System.out.println();
+
+
+            System.out.println("publisherID" + ", " + "publisherName");
+            while(query6.next()) {   // Move the cursor to the next row
+                int id = query6.getInt("publisherID");
+                String publisherName = query6.getString("publisherName");
+                System.out.println(id + ", " + publisherName);
+                ++rowCount;
+            }
+            System.out.println();
+            System.out.println();
+
+
+            // Edit/Update the existing information about a publishe
+            queryPrinter.setResult("UPDATE books.publishers SET publisherName = \"Shahbazi\" WHERE publisherID = 1;");
+            System.out.println("For the update: Edit/Update the existing information about an author");
+            System.out.println();
+            System.out.println("The updated records are:");
+            System.out.println();
+            // Print out the publishers table with the updated value
+            ResultSet query7 = queryPrinter.getResultSet("SELECT * FROM Books.publishers");
+
+            System.out.println("For the update: We print out publishers table after the addition");
+            System.out.println();
+            System.out.println("The updated records are:");
+            System.out.println();
+
+
+            System.out.println("publisherID" + ", " + "publisherName");
+            while(query7.next()) {   // Move the cursor to the next row
+                int id = query7.getInt("publisherID");
+                String publisherName = query7.getString("publisherName");
+                System.out.println(id + ", " + publisherName);
+                ++rowCount;
+            }
+
+
 
         }catch(SQLException se){
             //Handle errors for JDBC
@@ -56,9 +253,11 @@ public class Books {
                     conn.close();
             }catch(SQLException se){
                 se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-    }//end main
-}//end JDBCExample
+            }
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("System END");
+    }
+}
 
--- src/main/java/tableGenerator.java
+++ src/main/java/tableGenerator.java
@@ -1,8 +1,12 @@
 /**
  * Created by shahbazkhan on 11/30/15.
+ * 
+ *  This Class Generates Tables for OUr MySQL object and Populates them with random values.
  */
 
 import java.sql.*;
+import java.util.Random;
+
 public class tableGenerator {
     Statement stMt;
     Connection coNN;
@@ -10,32 +14,29 @@ public class tableGenerator {
         stMt=stmt;
         coNN=conn;
     }
+    
+    // This function creates the Tables
     public void createTable(){
-         /* String sql1 = "CREATE TABLE AUTHOR " +
-                  "(id INTEGER not NULL, " +
-                  " first VARCHAR(255), " +
-                  " last VARCHAR(255), " +
-                  " age INTEGER, " +
-                  " PRIMARY KEY ( id ))"; */
 
-        String sql1="CREATE TABLE authors "
-                + "(authorID INTEGER not NULL, "
+
+        String Table1="CREATE TABLE authors "
+                + "(authorID INTEGER not NULL auto_increment, "
                 + " first VARCHAR(255), "
                 + " last VARCHAR(255), "
                 + " PRIMARY KEY ( authorID ))";
 
-        String sql4="CREATE TABLE authorISBN "
+        String Table4="CREATE TABLE authorISBN "
                 + "(authorID INT, "
                 + "isbn CHAR(10), "
                 + "FOREIGN KEY ( authorID )REFERENCES authors( authorID ), "
                 + "FOREIGN KEY ( isbn )REFERENCES titles( isbn ))";
 
-        String sql2="CREATE TABLE publishers "
-                + "(publisherID INTEGER, "
+        String Table2="CREATE TABLE publishers "
+                + "(publisherID INTEGER not NULL auto_increment, "
                 + " publisherName char(100), "
                 + " PRIMARY KEY ( publisherID))";
 
-        String sql3="CREATE TABLE titles "
+        String Table3="CREATE TABLE titles "
                 + "(isbn CHAR(10), "
                 + " title VARCHAR(500), "
                 + " editionNumber INTEGER, "
@@ -45,15 +46,69 @@ public class tableGenerator {
                 + " PRIMARY KEY ( isbn ), "
                 + " FOREIGN KEY ( publisherID )REFERENCES publishers( publisherID ))";
         try{
-            stMt.executeUpdate(sql1);
-            stMt.executeUpdate(sql2);
-            stMt.executeUpdate(sql3);
-            stMt.executeUpdate(sql4);
+            // Executing the TABLE functions to add teh tables
+            stMt.executeUpdate(Table1);
+            stMt.executeUpdate(Table2);
+            stMt.executeUpdate(Table3);
+            stMt.executeUpdate(Table4);
         }catch(SQLException se){
             //Handle errors for JDBC
             se.printStackTrace();
-        }catch(Exception e){
+        }catch(Exception e) {
             //Handle errors for Class.forName
+            e.printStackTrace();
+        }
+    }
+
+    // A Random word generator.
+    public static String[] generateRandomWords(int numberOfWords)
+    {
+        String[] randomStrings = new String[numberOfWords];
+        Random random = new Random();
+        for(int i = 0; i < numberOfWords; i++)
+        {
+            char[] word = new char[random.nextInt(8)+3]; // words of length 3 through 10. (1 and 2 letter words are boring.)
+            for(int j = 0; j < word.length; j++)
+            {
+                word[j] = (char)('a' + random.nextInt(26));
+            }
+            randomStrings[i] = new String(word);
+        }
+        return randomStrings;
+    }
+
+    // This Function AutoPopulates the Tables.
+    public void autoPopulate()
+    {
+        try {
+            // Create Arrays of Random Words
+            String[] first = generateRandomWords(17);
+            String[] last = generateRandomWords(17);
+            String[] titles = generateRandomWords(17);
+            String[] publisherName = generateRandomWords(17);
+            
+            // Loop through 16 times and Auto fill all table values
+            for (int i=1; i<16;i++)
+            {
+                String inAuthor = "INSERT INTO books.authors (authorID, first, last) VALUES (" + i+ ", '" + first[i] +"', '"+last[i]+"');";
+
+                stMt.execute(inAuthor);
+
+                String inPublisher = "INSERT INTO Books.publishers (publisherID, publisherName) VALUES (" + i + ", '" + publisherName[i] + "');";
+
+                stMt.execute(inPublisher);
+
+                String inTitles = "INSERT INTO Books.titles (isbn, title,publisherID, copyright) VALUES ('isbn" + i + "', '" + titles[i] + "', " +i +" , "+ (2000+i)+");";
+
+                stMt.execute(inTitles);
+
+                String inISBN = "INSERT INTO Books.authorISBN (authorID, isbn) VALUES (" + i + ", 'isbn" + i + "');";
+
+                stMt.execute(inISBN);
+
+            }
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
 
